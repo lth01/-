d81f3f169e22135ba5eb363bf1ac9ffe1d1e3917
lth01 <dlxogml11235@naver.com>
2021-04-11 23:10:18 +0900

    LRU 구현완료

---
+++ LRU_algorithm/LRU.py
@@ -0,0 +1,67 @@
+#LRU Algorithm #2
+#이전 알고리즘은 시간 복잡도가 높아 캐시가 많을경우 최대 1시간까지도 실행시간이 걸렸었다.
+
+class Cache_memory:
+
+    def __init__(self,_val,_prev=None,_next=None):
+
+        self.val=_val
+        self.prev=_prev
+        self.next=_next
+
+    def Cache_update(self): #좌우 노드들의 연결을 서로 시키고 나는 맨앞으로 이동 head,tail의 앞뒤에 있는 노드는 아니라고 가정
+        tmp=self.prev
+        (self.next).prev=tmp #다음 노드의 이전 값을 tmp로 설정
+        (self.prev).next=self.next
+        self.next=Cache_head.next
+        self.prev=Cache_head
+        Cache_head.next=self
+
+
+
+
+
+def _init_Cache(head,tail):
+    head.next=tail
+    tail.prev=head
+
+def find(key,map): #map의 node값이 None이 아니면 key값을 리턴한다. 만약 None이라면 -1을 리턴한다.(리스트안에 없다)
+    if(map[key][0]!=None):
+        return key
+    else:
+        return -1
+
+
+CACHE_SIZE = 10000
+  # cache size를 미리 정해준다.
+
+Hit_count =0
+Miss_count =0
+Cache_request = 0  # id 값을 불러와 임시적으로 저장할 변수
+
+map=dict() #dict은 key:[node,count]로 관리될 것이다.
+
+Cache_head=Cache_memory(-1)
+Cache_tail=Cache_memory(-1)
+
+_init_Cache(Cache_head,Cache_tail)
+
+
+
+F = open("../../../Downloads/algorithm-master/request.tr", 'r')  # f라는 객체에 파일 request.tr을 일여놓는다.
+
+while(True):
+    Cache_request=F.readline()
+    if not Cache_request:
+        break #""를 읽어오면 EOF이므로 종료한다.
+    else:
+        Cache_request=int(Cache_request)#읽어온 str문자열을 정수로 바꾼다.
+    index=find(Cache_request, Cache_memory)
+    if(index>=0):#이미 캐시메모리 안에 해당 값이 있다면 그 값의 index=0으로한다 삭제 x
+        tmp=Cache_memory[index]
+        del Cache_memory[index]
+        append(tmp)
+        Hit_count+=1
+    else:#캐시메모리에 index가 존재하지 않는다면 Cache_memory의 첫번째 인덱스를 삭제하고, 맨 마지막에 값을 추가해준다.
+        append(Cache_request)
+        Miss_count+=1
---
+++ LRU_algorithm/main.py
@@ -0,0 +1,103 @@
+class Node:
+    def __init__(self,data,prev=None,next=None):
+        self.prev= prev
+        self.data= data
+        self.next= next
+class List:#node들을 관리해준다.
+    def find(self,head,map,val):
+        if(map.get(val)==None):
+            return -1
+        else:
+            return map.get(val)
+
+    def update(self,head,map,val,count_map):#val을 찾아 첫번째 노드로 가져온다.
+        node=self.find(head,map,val)
+        if(node==-1): #노드가 없었다면
+            return -1
+        else:
+            if(node.next):
+                tmp_node=node.next
+                tmp_node.prev=node.prev
+                node.prev.next=tmp_node
+                #노드의 연결을 끊었으므로 head.next node로 설정한다.
+                tmp_node=head.next
+                head.next=node
+                node.prev=head
+                node.next=tmp_node
+                tmp_node.prev=node
+                if(count_map.get(val)==None):
+                    count_map[val]=1
+                else:
+                    count_map[val]+=1
+
+    def delete(self,head,tail,map):#해당 함수는 tail만 사용할 수 있다.
+        if(tail.next==None):
+            tmp_node=tail.prev
+            tail.prev=tmp_node.prev
+            tmp_node.prev.next=tail
+            del map[tmp_node.data]
+            head.data-=1
+        else:
+            print("is not tail node")
+
+    def append(self,head,map,val):
+        if(map.get(val)==None):#해당 map에 노드가 없다면
+            node=Node(val)
+            map[val]=node
+        else:
+            node=map.get(val)
+        if(head.next!=None):
+            tmp_node=head.next
+            head.next=node
+            node.prev=head
+            node.next=tmp_node
+            tmp_node.prev=node
+            head.data+=1
+
+def _init(head,tail):
+    head.next=tail
+    head.prev=None
+    tail.prev=head
+    tail.next=None
+
+CACHE_SIZE=3
+Cache_head=Node(0)
+Cache_tail=Node(-1)
+
+_init(Cache_head,Cache_tail)
+
+list=List()
+map=dict() # key:node
+count_map=dict() #key:hit_count
+total_count=0
+total_hit_count=0
+popular_values=-1
+index=0
+F = open("request1.tr", 'r')
+
+
+while(True):
+    Cache_request = F.readline()
+    if not Cache_request:
+        break  # ""를 읽어오면 EOF이므로 종료한다.
+    else:
+        Cache_request = int(Cache_request)  # 읽어온 str문자열을 정수로 바꾼다.
+    total_count+=1
+    types=list.update(Cache_head,map,Cache_request,count_map)
+    if(type(types)==int): #읽어온 값이 없었다면
+        list.append(Cache_head,map,Cache_request)
+    if(Cache_head.data>CACHE_SIZE):
+        list.delete(Cache_head,Cache_tail,map)
+
+for val in count_map.keys():
+    tmp=count_map.get(val)
+    if(popular_values<tmp):
+        popular_values=tmp
+        index=val#해당 맵의 키값저장
+    total_hit_count+=tmp
+
+print("Result of using LRU")
+print("Hit :",total_hit_count)
+print("Miss :",total_count-total_hit_count)
+print("Most popular data ID :",index)
+print("The number of hits for the most popular ID :",count_map.get(index))
